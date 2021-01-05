91d46c40d1a0eaf7e13894b33ca99007534b4d1a
Trilochan Tripathi <trilochan-prasad.tripathi@bbc.com>
2021-01-05 14:24:40 +0530

    feat: add navigation link

--- angular/src/app/shared/layout/nav/app-navigation.service.ts
+++ angular/src/app/shared/layout/nav/app-navigation.service.ts
@@ -33,6 +33,7 @@ export class AppNavigationService {
                 new AppMenuItem('Settings', 'Pages.Administration.Host.Settings', 'flaticon-settings', '/app/admin/hostSettings'),
                 new AppMenuItem('Settings', 'Pages.Administration.Tenant.Settings', 'flaticon-settings', '/app/admin/tenantSettings')
             ]),
+			new AppMenuItem('Go To Grid', '', 'flaticons-settings', '/app/admin/demo-ui-components')
             new AppMenuItem('DemoUiComponents', 'Pages.DemoUiComponents', 'flaticon-shapes', '/app/admin/demo-ui-components')
         ]);
     }
